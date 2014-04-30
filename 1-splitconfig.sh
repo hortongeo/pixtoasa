e0cdf79436b8c8ae0a3a8df3ced346ed2adffca6
ghorton <george@ifl.net>
2014-04-30 13:22:13 +0100

    added splitconfig

---
+++ 1-splitconfig.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Take a PIX 8.0 configuration file and split it into component parts
+#
+# George Horton - 05/2014
+#
+
+# Global
+CONFIG=$1
+if [ ! -e "$CONFIG" ]
+then
+	echo "Usage: 1-splitconfig.sh <PIX_CONFIG_FILE>"
+	exit 1;
+fi
+
+mkdir PIX
+
+# Name
+egrep '^name ' $CONFIG > PIX/name
+
+# Objects
+egrep "^object " $CONFIG > PIX/object
+
+# object-group
+rm -f PIX/object-group
+START=`egrep "^object-group " $CONFIG -n | head -1 | cut -d ":" -f 1`
+CONTUNIE=1
+
+while [ $CONTUNIE -eq 1 ]
+do
+	LINE=`sed -n "$START"p $CONFIG`
+
+	TMP=`echo "$LINE" | egrep "^ "`
+	if [ $? -eq 0 ]
+	then
+		echo "$LINE" >> PIX/object-group
+		let START=$START+1
+	else
+		TMP=`echo "$LINE" | egrep "^object-group "`
+		if [ $? -eq 0 ]
+		then
+			echo "$LINE" >> PIX/object-group
+			let START=$START+1
+		else
+			CONTUNIE=0
+		fi
+	fi
+done
+
+# ACL
+ACLS=`egrep "^access-group " $CONFIG | cut -d " " -f 2 | sort | uniq`
+for ACL in $ACLS
+do
+	egrep "^access-list $ACL " $CONFIG > PIX/$ACL
+done
